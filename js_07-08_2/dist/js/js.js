4773c0e11b90d487e42908328d1fa2f7f9e3545e
andry <shapovala@ukr.net>
2016-07-11 10:54:05 +0300

    new dz

---
+++ js_07-08_2/dist/js/js.js
@@ -0,0 +1,80 @@
+﻿jQuery(function($) {
+  
+		
+
+	(function appendText() {            
+		var $pop = $('<div class="pop"></div>');   
+	    $(".form-group").append($pop);
+	    $("#exampleInputName").next('.pop').text('Please provide your firstname');
+	    $("#exampleInputLastName").next('.pop').text('Please provide also your lastname');
+	    $("#exampleInputAddress").next('.pop').text('Your home or work adress');
+
+	    var $target = $(".form-group input");
+
+	    $($target).hover(
+		  function() {
+		    $(this).next('.pop').animate({
+				    top: "28px",
+				    opacity: 1
+				  }, 500 );
+
+		  }, function() {
+
+		  	$(this).next('.pop').animate({
+				    top: "-28px",
+				    opacity: 0
+				  }, 500 );
+		   
+		  }
+		);
+
+	    $('.btn').on('click', function(e){
+			e.preventDefault();
+			$('.pop').animate({
+				    top: "28px",
+				    opacity: 1
+				  }, 500 );
+
+			
+		});
+
+		$($target).focus(function() {
+			$(this).next('.pop').animate({
+				    top: "28px",
+				    opacity: 1
+				  }, 500 );
+		});
+
+		$($target).blur(function() {
+			$(this).next('.pop').animate({
+				    top: "-28px",
+				    opacity: 0
+				  }, 500 );
+		});
+
+
+
+	}());
+
+
+});
+
+
+
+
+
+	 
+	 
+
+	
+
+	
+
+	
+
+
+   
+
+
+   
+    
