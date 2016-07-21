cafb1081aa5f89eee11b429fdff65ce059729946
andry <shapovala@ukr.net>
2016-07-21 11:23:34 +0300

    dz

---
+++ js_15-16_1/dist/js/js.js
@@ -0,0 +1,21 @@
+﻿$(function(){
+
+	
+
+	$.ajax({
+	  url: "https://pixabay.com/api/?key=2953875-64107f3e4a827b9cba547e216&q=yellow+flowers&image_type=photo",
+	  dataType : "jsonp",    
+      success: function (data) {
+           
+
+            var images = document.getElementById("images");
+    		images.innerHTML = tmpl("pics", data);
+        }
+	  });
+
+
+
+  
+
+
+});
