0ed7c26ef0c3c29da8425e1e302362f14c3a442f
andry <shapovala@ukr.net>
2016-07-27 10:47:33 +0300

    dz

--- js_15-16_1/dist/js/js.js
+++ js_15-16_1/dist/js/js.js
@@ -1,17 +1,82 @@
 ﻿$(function(){
 
+		
 	
 
-	$.ajax({
-	  url: "https://pixabay.com/api/?key=2953875-64107f3e4a827b9cba547e216&q=yellow+flowers&image_type=photo",
-	  dataType : "jsonp",    
-      success: function (data) {
-           
+	
 
-            var images = document.getElementById("images");
-    		images.innerHTML = tmpl("pics", data);
-        }
-	  });
+
+	
+
+	$( ".btn" ).on( "click", function(event) {
+  		event.preventDefault();
+  		
+  		var valuetext = $('#textInput').val();
+
+  		if ( $('#textInput').val() == '' ) {
+			$('.list').hide();
+			$('#images').text('Введите запрос в поле поиска');
+		} else {
+
+	  		$.ajax({
+			  url: "https://pixabay.com/api/?key=2953875-64107f3e4a827b9cba547e216&q="+ valuetext +"&image_type=all",
+			  dataType : "jsonp",    
+		      success: function (data) {
+		           
+
+		            var images = document.getElementById("images");
+		    		images.innerHTML = tmpl("pics", data);
+		        }
+			  });
+
+  		}
+  		
+
+	});
+
+	$(document).keypress(function(e){
+		if(e.which == 13) {
+
+			
+			var valuetext = $('#textInput').val();
+
+	  		if ( $('#textInput').val() == '' ) {
+				$('.list').hide();
+				$('#images').text('Введите запрос в поле поиска');
+			} else {
+
+		  		$.ajax({
+				  url: "https://pixabay.com/api/?key=2953875-64107f3e4a827b9cba547e216&q="+ valuetext +"&image_type=all",
+				  dataType : "jsonp",    
+			      success: function (data) {
+			           
+
+			            var images = document.getElementById("images");
+			    		images.innerHTML = tmpl("pics", data);
+			        }
+				  });
+
+	  		}
+
+
+    	}
+	    
+	});
+
+
+
+
+
+
+	
+
+	
+	
+
+	
+
+	
+
 
 
 
