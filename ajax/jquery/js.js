72b980354f778810912900d08bd3d044a28ccd63
Andry85 <shapovala@ukr.net>
2019-10-04 18:05:29 +0300

    aded

--- ajax/jquery/js.js
+++ ajax/jquery/js.js
@@ -4,5 +4,17 @@ $(document).ready(function(){
         url: "https://repetitora.net/api/JS/Images", 
         success: function(result){
             console.log(result);
+            result.forEach(function(item, i, arr) {
+			   const img = document.createElement('img');
+			   img.src = item.thumbnail;
+			   const link = document.createElement('a');
+			   link.href = item.original;
+			   link.target = '_blank';
+			   link.appendChild(img);
+			  
+			  document.querySelector('body').appendChild(link);
+			});
+
+
         }});
   });
