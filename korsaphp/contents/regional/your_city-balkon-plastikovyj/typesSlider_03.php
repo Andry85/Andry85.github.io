98f0216f077da47bdaf1284e9f16db72fb166172
Andry85 <shapovala@ukr.net>
2019-10-07 17:44:18 +0300

    balkon added page

---
+++ korsaphp/contents/regional/your_city-balkon-plastikovyj/typesSlider_03.php
@@ -0,0 +1,101 @@
+<div class="listType_1">
+
+	<div class="listType_1__item">
+		<div class="listType_1__item__info">
+			<div class="listType_1__item__discount">
+	            <p>Экономия</p>
+	            <h5>4312 грн</h5>
+	        </div>
+	    </div>
+		<figure class="listType_1__pic">
+			<div class="listType_1__img">
+				<img src="uploads/your_city-balkon-plastikovyj/slider_03/001.jpg" alt="">
+			</div>
+			<figcaption class="listType_1__title">Балконный блок, дверь с панелью и окно с открыванием</figcaption>
+		</figure>
+		<div class="listType_1__footer">
+			<p class="asPrice_withoutDiscount">Цена без скидки от <span class="line-through">14372 грн</span></p>
+			<div class="asPrice">
+				Цена от
+				<span class="asPrice__count">10060</span>
+				<span class="asPrice__currency">грн</span>
+			</div>
+			<a class="js-callModal asBtnLink asBtnLink--primary" href="#" title="ЗАЯВКА НА ПОКУПКУ балкона">Заказать</a>
+		</div>
+	</div>
+
+	<div class="listType_1__item">
+		<div class="listType_1__item__info">
+			<div class="listType_1__item__discount">
+	            <p>Экономия</p>
+	            <h5>4312 грн</h5>
+	        </div>
+	    </div>
+		<figure class="listType_1__pic">
+			<div class="listType_1__img">
+				<img src="uploads/your_city-balkon-plastikovyj/slider_03/002.jpg" alt="">
+			</div>
+			<figcaption class="listType_1__title">Балконный блок, дверь со стеклом и глухое окно без открывания</figcaption>
+		</figure>
+		<div class="listType_1__footer">
+			<p class="asPrice_withoutDiscount">Цена без скидки от <span class="line-through">14372 грн</span></p>
+			<div class="asPrice">
+				Цена от
+				<span class="asPrice__count">10060</span>
+				<span class="asPrice__currency">грн</span>
+			</div>
+			<a class="js-callModal asBtnLink asBtnLink--primary" href="#" title="ЗАЯВКА НА ПОКУПКУ балкона">Заказать</a>
+		</div>
+	</div>
+
+	<div class="listType_1__item">
+		<div class="listType_1__item__info">
+			<div class="listType_1__item__discount">
+	            <p>Экономия</p>
+	            <h5>4312 грн</h5>
+	        </div>
+	    </div>
+		<figure class="listType_1__pic">
+			<div class="listType_1__img">
+				<img src="uploads/your_city-balkon-plastikovyj/slider_03/003.jpg" alt="">
+			</div>
+			<figcaption class="listType_1__title">Балконный блок, дверь с панелью и окно из двух частей с открыванием</figcaption>
+		</figure>
+		<div class="listType_1__footer">
+			<p class="asPrice_withoutDiscount">Цена без скидки от <span class="line-through">14372 грн</span></p>
+			<div class="asPrice">
+				Цена от
+				<span class="asPrice__count">10060</span>
+				<span class="asPrice__currency">грн</span>
+			</div>
+			<a class="js-callModal asBtnLink asBtnLink--primary" href="#" title="ЗАЯВКА НА ПОКУПКУ балкона">Заказать</a>
+		</div>
+	</div>
+
+	<div class="listType_1__item">
+		<div class="listType_1__item__info">
+			<div class="listType_1__item__discount">
+	            <p>Экономия</p>
+	            <h5>4312 грн</h5>
+	        </div>
+	    </div>
+		<figure class="listType_1__pic">
+			<div class="listType_1__img">
+				<img src="uploads/your_city-balkon-plastikovyj/slider_03/003.jpg" alt="">
+			</div>
+			<figcaption class="listType_1__title">Балконный блок, дверь с панелью и окно из двух частей с открыванием</figcaption>
+		</figure>
+		<div class="listType_1__footer">
+			<p class="asPrice_withoutDiscount">Цена без скидки от <span class="line-through">14372 грн</span></p>
+			<div class="asPrice">
+				Цена от
+				<span class="asPrice__count">10060</span>
+				<span class="asPrice__currency">грн</span>
+			</div>
+			<a class="js-callModal asBtnLink asBtnLink--primary" href="#" title="ЗАЯВКА НА ПОКУПКУ балкона">Заказать</a>
+		</div>
+	</div>
+
+	
+
+</div>
---
+++ korsaphp/contents/regional/your_city-balkon-plastikovyj/videoCatalog.php
@@ -0,0 +1,48 @@
+<div class="videoCatalog">
+	<div class="row">
+		<div class="col-md-6">
+			<article class="videoBlock">
+				<h2 class="as__subtitle">видео: Розсувні балкони з ПВХ - KORSA SLIDING SYSTEM</h2>
+				<div class="videoBlock__source">
+					<iframe width="560" height="315" src="https://www.youtube.com/embed/hfI5KRjOdG8" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+				</div>
+			</article>
+		</div>
+		<div class="col-md-6">
+			<article class="videoBlock">
+				<h2 class="as__subtitle">Видео: Більше світла - балкони REHAU EURO 70</h2>
+				<div class="videoBlock__source">
+					<iframe width="560" height="315" src="https://www.youtube.com/embed/_xlmRDE251Q" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+				</div>
+			</article>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-6">
+			<article class="videoBlock">
+				<h2 class="as__subtitle">Видео: Доступне скління - балкони REHAU ECOSOL 60</h2>
+				<div class="videoBlock__source">
+					<iframe width="560" height="315" src="https://www.youtube.com/embed/ptm2TksD7lE" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+				</div>
+			</article>
+		</div>
+		<div class="col-md-6">
+			<article class="videoBlock">
+				<h2 class="as__subtitle">Видео: Оптимальний варіант скління - балкони REHAU KORSA DESIGN 70</h2>
+				<div class="videoBlock__source">
+					<iframe width="560" height="315" src="https://www.youtube.com/embed/4TVtUSUKNcc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+				</div>
+			</article>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-6">
+			<article class="videoBlock">
+				<h2 class="as__subtitle">Видео: Балкони REHAU BRILLANT DESIGN</h2>
+				<div class="videoBlock__source">
+					<iframe width="560" height="315" src="https://www.youtube.com/embed/7YzoRikoDMc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+				</div>
+			</article>
+		</div>
+	</div>
+</div>
