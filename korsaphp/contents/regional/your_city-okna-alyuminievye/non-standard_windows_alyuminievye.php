27cd158f1a06515f1409448f989c5d5fd370b327
Andry85 <shapovala@ukr.net>
2019-10-15 12:47:10 +0300

    your_city-okna-alyuminievye.php done

---
+++ korsaphp/contents/regional/your_city-okna-alyuminievye/non-standard_windows_alyuminievye.php
@@ -0,0 +1,59 @@
+<section class="asProductBlock">
+	<h2 class="as__subtitle">нестандартные формы алюминиевых окон</h2>
+	<div class="row">
+		<div class="col-md-12">
+			<ul class="nonStandardList">
+				<li class="nonStandardList__item">
+					<a class="nonStandardList__link" href="#">
+						<figure class="nonStandardList__pic">
+							<div class="nonStandardList__img">
+								<img src="img/polygon_window.png" alt="Многоугольное окно">
+							</div>	
+							<figcaption>Многоугольное окно</figcaption>
+						</figure>
+					</a>
+				</li>
+				<li class="nonStandardList__item">
+					<a class="nonStandardList__link" href="#">
+						<figure class="nonStandardList__pic">
+							<div class="nonStandardList__img">
+								<img src="img/trapezoidal_window.png" alt="Трапециевидное окно">
+							</div>	
+							<figcaption>Трапециевидное окно</figcaption>
+						</figure>
+					</a>
+				</li>
+				<li class="nonStandardList__item">
+					<a class="nonStandardList__link" href="#">
+						<figure class="nonStandardList__pic">
+							<div class="nonStandardList__img">
+								<img src="img/triangular_window.png" alt="Треугольное окно">
+							</div>	
+							<figcaption>Треугольное окно</figcaption>
+						</figure>
+					</a>
+				</li>
+				<li class="nonStandardList__item">
+					<a class="nonStandardList__link" href="#">
+						<figure class="nonStandardList__pic">
+							<div class="nonStandardList__img">
+								<img src="img/square_window.png" alt="Квадратное окно">
+							</div>	
+							<figcaption>Квадратное окно</figcaption>
+						</figure>
+					</a>
+				</li>
+				<li class="nonStandardList__item">
+					<a class="nonStandardList__link" href="#">
+						<figure class="nonStandardList__pic">
+							<div class="nonStandardList__img">
+								<img src="img/triangular_window_version_two.png" alt="Треугольное окно">
+							</div>	
+							<figcaption>Треугольное окно</figcaption>
+						</figure>
+					</a>
+				</li>
+			</ul>				
+		</div>
+	</div>
+</section>
---
+++ korsaphp/contents/regional/your_city-okna-alyuminievye/slider.php
@@ -0,0 +1,36 @@
+<div class="asCarousel">
+	<div class="owl-carousel">
+		<div class="asCarousel__item">
+			<a href="uploads/your_city-okna-alyuminievye/001.jpg" data-fancybox="plascicImages-1">
+				<img src="uploads/your_city-okna-alyuminievye/001.jpg" alt="">
+			</a>	
+			<a href="uploads/your_city-okna-alyuminievye/002.jpg" data-fancybox="plascicImages-1">
+				<img src="uploads/your_city-okna-alyuminievye/002.jpg" alt="">
+			</a>
+		</div>
+		<div class="asCarousel__item">	
+			<a href="uploads/your_city-okna-alyuminievye/003.jpg" data-fancybox="plascicImages-1">
+				<img src="uploads/your_city-okna-alyuminievye/003.jpg" alt="">
+			</a>	
+			<a href="uploads/your_city-okna-alyuminievye/004.jpg" data-fancybox="plascicImages-1">
+				<img src="uploads/your_city-okna-alyuminievye/004.jpg" alt="">
+			</a>
+		</div>
+		<div class="asCarousel__item">
+			<a href="uploads/your_city-okna-alyuminievye/005.jpg" data-fancybox="plascicImages-1">
+				<img src="uploads/your_city-okna-alyuminievye/005.jpg" alt="">
+			</a>	
+			<a href="uploads/your_city-okna-alyuminievye/006.jpg" data-fancybox="plascicImages-1">
+				<img src="uploads/your_city-okna-alyuminievye/006.jpg" alt="">
+			</a>
+		</div>
+		<div class="asCarousel__item">	
+			<a href="uploads/your_city-okna-alyuminievye/007.jpg" data-fancybox="plascicImages-1">
+				<img src="uploads/your_city-okna-alyuminievye/007.jpg" alt="">
+			</a>	
+			<a href="uploads/your_city-okna-alyuminievye/008.jpg" data-fancybox="plascicImages-1">
+				<img src="uploads/your_city-okna-alyuminievye/008.jpg" alt="">
+			</a>
+		</div>
+	</div>
+</div>
