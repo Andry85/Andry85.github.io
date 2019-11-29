20b76c7d2bf0be2187d3910c445c8aa9dc1b256b
Andry85 <shapovala@ukr.net>
2019-11-29 09:50:36 +0200

    added steklyannye peregorodki

---
+++ korsaphp/your_city-peregorodki-steklyannye.php
@@ -0,0 +1,112 @@
+<?php include 'headers/header.php'; ?>
+
+<div class="container">
+	<div class="row lg-pb-10">
+		<div class="col-md-12">
+		
+			<section class="asProductDesc">
+				<h1 class="asProductDesc__title">Стеклянные перегородки В КИЕВЕ</h1>
+				<ul class="breadcrumb">
+					<li class="breadcrumb__item">
+						<a class="breadcrumb__link" href="index.php"><i class="icons fa fa-home"></i></a>
+					</li>
+					<li class="breadcrumb__item">
+						<span>купить стеклянные перегородки киев</span>
+					</li>
+				</ul>
+				<div class="asProductDesc_text asProductDesc_text--nobotomMargin">
+					<p class="asProductDescText__entry">
+						Стеклянные перегородки в Киеве - эффектное, современное решение для обустройства жилых, офисных помещений. Их популярность обусловлена низкой массой, высокой светопропускной способностью, привлекательным внешним видом. Чтобы заказать конструкции из алюминия по доступной цене, теперь не придется тратить время на поиски магазинов. Достаточно найти на сайте ближайший фирменный магазин, чтобы получить квалифицированную консультацию, ознакомиться ассортимент, заказать подходящий вариант. Для комфорта пользователей на сайте размещена карта, которая поможет увидеть расположение офиса и проложить маршрут к нему.
+					</p>
+				</div>
+			</section>
+			<?php include 'contents/regional/your_city/whereToBuy.php'; ?>
+			<?php include 'contents/regional/your_city/map.php'; ?>
+			<div class="row">
+				<div class="col-md-12">
+					<div class="asProductDesc_text asProductDesc_text--nobotomMargin">
+						<p class="asProductDescText__entry">Компания Корса работает в Украине с 1997 года, и имеет богатый опыт более 20 лет в производстве алюминиевой, металлопластиковой продукции. Предлагает купить перегородки в Киеве и области, а также в других городах Украины. Сейчас  работает порядка 120 фирменных магазинов компании, в которых предлагается продукция из алюминия ALUTECH(Беларусь) и ALTEST (Болгария). Профиль может без  утепления (холодный алюминий) или оснащаться терморазрывом для снижения затрат на отопление (теплый алюминий).</p>
+						<p class="asProductDescText__entry">ТМ Корса использует импортные комплектующие, фурнитуру при выпуске продукции, обеспечивает четкий контроль качества на каждом этапе. Офисные перегородки, предлагаемые брендом, могут быть укомплектованы противовзломными системами.</p>	
+					</div>
+				</div>
+			</div>
+
+		</div>
+	</div>
+</div>
+
+<div class="container">
+	<div class="row">
+		<div class="col-md-12">
+			<section class="asProductBlock lg-pb-5">
+				<h2 class="as__subtitle">Стеклянные перегородки в Киеве</h2>
+				<div class="row">
+					<div class="col-md-12">
+						<div class="asProductBlock__content">
+							<p class="asProductBlock__text">Компания Корса работает в Украине с 1997 года, и имеет богатый опыт более 20 лет в производстве алюминиевой, металлопластиковой продукции. Предлагает купить перегородки в Киеве и области, а также в других городах Украины. Сейчас  работает порядка 120 фирменных магазинов компании, в которых предлагается продукция из алюминия ALUTECH(Беларусь) и ALTEST (Болгария). Профиль может без  утепления (холодный алюминий) или оснащаться терморазрывом для снижения затрат на отопление (теплый алюминий).</p>
+							<p class="asProductBlock__text">ТМ Корса использует импортные комплектующие, фурнитуру при выпуске продукции, обеспечивает четкий контроль качества на каждом этапе. Офисные перегородки, предлагаемые брендом, могут быть укомплектованы противовзломными системами.</p>
+						</div>
+					</div>
+				</div>
+			</section>
+
+			<?php include 'contents/regional/your_city-peregorodki/canbeused.php'; ?>
+
+			
+			<section class="asProductBlock lg-pt-45 lg-pb-10">
+				<h2 class="as__subtitle">Цены на стеклянные перегородки</h2>
+				<div class="row">
+					<div class="col-md-12">
+						<?php include 'contents/regional/your_city-peregorodki-steklyannye/typesSlider.php'; ?>
+					</div>
+				</div>
+			</section>
+			<?php include 'contents/advantagesSecond.php'; ?>
+			<div class="row typesSlider-alyuminievye">
+				<div class="col-md-12">
+					<?php include 'contents/regional/your_city-peregorodki-steklyannye/typesSlider--second.php'; ?>
+					<p class="asProductBlock__text">Принимая решение заказать алюминиевые перегородки в Киеве по приемлемой цене, не стоит думать, что перед вами предстанет исключительно металлический блеск, который сочетается с прозрачным стеклом. Разнообразие современного покрытия позволяет получить модели различных оттенков, имитирующие фактуру дерева и разных материалов на выбор.</p>
+					<p class="asProductBlock__text">Обратившись в ближайшую точку продаж, легко получить дельную рекомендацию, помощь по выбору оптимальной конструкции, а также оставить заявку. Благодаря собственной логистике, заказ будет доставлен в кратчайшие сроки.</p>
+					<div class="lg-pt-20 lg-pb-25">
+						<?php include 'contents/regional/your_city-peregorodki-steklyannye/slider.php'; ?>
+					</div>	
+				</div>
+			</div>
+			<div class="lg-ptb-25">
+			<?php include 'contents/regional/your_city-peregorodki/offer_list--doors.php'; ?>
+			</div>
+		</div>
+	</div>
+</div>
+
+<?php include 'contents/getCall_first.php'; ?>
+
+<div class="container">
+	<div class="row lg-pt-65">
+		<div class="col-md-12">
+			<?php include 'contents/regional/your_city-peregorodki/profits.php'; ?>
+			<div class="lg-pt-25">
+				<?php include 'contents/popularСolors.php'; ?>
+			</div>	
+		</div>
+	</div>
+</div>	
+
+
+
+<?php include 'contents/our_offices.php'; ?>
+
+<div class="container">
+	<div class="row lg-pt-60">
+		<div class="col-md-12">
+			<?php include 'contents/regional/your_city-peregorodki-steklyannye/videoCatalog.php'; ?>
+		</div>
+	</div>
+</div>
+
+<?php include 'contents/regional/your_city-peregorodki-steklyannye/portfolio.php'; ?>
+<?php include 'contents/reviews.php'; ?>
+<?php include 'contents/recommends.php'; ?>
+
+<?php include 'contents/navi.php'; ?>
+<?php include 'footers/footer.php'; ?>
