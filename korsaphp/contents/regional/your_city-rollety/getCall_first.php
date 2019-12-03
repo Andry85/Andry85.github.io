f984daec85b75c53637948429fac4f7edfe6c4bc
Andry85 <shapovala@ukr.net>
2019-12-03 18:09:13 +0200

    added rollety

---
+++ korsaphp/contents/regional/your_city-rollety/getCall_first.php
@@ -0,0 +1,21 @@
+<div class="getCallWrap">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<h2 class="getCallWrap__title">поможем подобрать<br> защитные роллеты на окна и двери</h2>
+				<p class="getCallWrap__text">Получите бесплатную консультацию по выбору защитных роллет на окна и двери. Мы предложим оптимальный вариант исходя из ваших предпочтений</p>
+				<div class="getCallFormWrap">
+					<form id="getCallForm_1" method="post" action="#" class="getCallForm">
+						<div class="getCallForm__col">
+							<input class="getCallForm__field" type="text" name="getcalfirstname__getCallForm_1" placeholder="введите имя" required="required">
+						</div>
+						<div class="getCallForm__col">
+							<input class="getCallForm__field" type="tel" name="getcalphone__getCallForm_1" placeholder="введите телефон" required="required">
+						</div>
+						<button class="getCallForm__btn">нужна консультация</button>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>	
---
+++ korsaphp/your_city-rollety.php
@@ -0,0 +1,136 @@
+<?php include 'headers/header.php'; ?>
+
+<div class="container">
+	<div class="row">
+		<div class="col-md-12">
+			
+			<section class="asProductDesc">
+				<h1 class="asProductDesc__title">ЗАщитные роллеты и решетки В КИЕВЕ</h1>
+				<ul class="breadcrumb">
+					<li class="breadcrumb__item">
+						<a class="breadcrumb__link" href="index.php"><i class="icons fa fa-home"></i></a> 
+					</li>
+					<li class="breadcrumb__item">
+						<span>купить защитные роллеты и решетки киев</span>
+					</li>
+				</ul>
+				<div class="asProductDesc_text asProductDesc_text--nobotomMargin">
+					<p class="asProductDescText__entry">
+						На сайте нашей компании представлен широкий ассортимент перегородок для офиса, ресторана, магазина, торгового центра, дома и дачи. Мы предлагаем стеклянные, металлопластиковые, алюминиевые перегородки, которые отменно вписываются в любой интерьер. Эстетичные и прочные конструкции изготавливаются нашими заводами с европейским оборудованием. Используются новейшие технологии. Для вашего удобства на сайте представлены ближайшие пункты продаж. С помощью интерактивной карты вы узнаете кратчайший путь к офису. И сможете купить перегородки в Киеве по ценам производителя. Специалисты подробнее расскажут о продукции, ее качестве, особенностях. Помогут определиться с наилучшим выбором. Профессионально ответят на все волнующие вопросы относительно товара и дизайна. 
+					</p>
+				</div>
+			</section>
+
+			
+			<?php include 'contents/regional/your_city/whereToBuy.php'; ?>
+			<?php include 'contents/regional/your_city/map.php'; ?>
+
+			<div class="row lg-mb-20">
+				<div class="col-md-12">
+					<div class="asProductDesc_text asProductDesc_text--nobotomMargin">
+						<p class="asProductDescText__entry">Компания Окна КОРСА вышла на рынок в 1997-ом году и сразу же заняла лидирующие позиции благодаря высокому качеству продукции. Лучшим доказательством надежности является обширная база постоянных клиентов, их положительные отзывы. Покупатели, однажды заказав пластиковые перегородки, впоследствии обращаются только в нашу компанию. Офисные перегородки и двери изготавливаются по заказу клиентов, с учетом общего интерьера помещения.</p>	
+					</div>
+				</div>
+			</div>
+
+			<?php include 'contents/regional/your_city-rollety/offer_list.php'; ?>
+
+			<section class="asProductBlock">
+				<h2 class="as__subtitle">Защитные роллеты в Киеве</h2>
+				<div class="row">
+					<div class="col-md-6">
+						<div class="asProductBlock__content">
+							<p class="asProductBlock__text">Роллеты (рольставни) ТМ КОРСА предназначены для дополнительной защиты помещения от взлома, посторонних взглядов,  влияния погодных условий, обеспечивая, в тоже время, ощутимое сбережение энергии. Роллеты устанавливаются снаружи помещения, обычно вплотную к окнам.</p>
+							<p class="asProductBlock__text">Роллеты поднимаются / опускаются путем скручивания роллеты на вал, расположенный в коробе сверху окна. Возможна дополнительная фиксация роллеты в закрытом состоянии путем установления в нижней части ролеты механического замка. Роллеты состоят из профилей (ламелей), короба, в который скручивается роллета, направляющих шин и механизма управления.</p>
+							<p class="asProductBlock__text">Существует несколько вариантов  механизмов управления роллетами: инерционный, шнуровой, воротковый, пружинно-инерционный. Выбор механизма роллет зависит от веса роллетного полотна. Само роллетное полотно состоит из роллетных профилей (ламелей). Роллетные профили делятся на пенозаполенные и экструдированные. Короб и боковые крышки. Короб роллет ТМ КОРСА изготавливается  в различных формах, и может быть выполнен как с углами, так и с закруглением. Внутрь короба сворачивается поднимающаяся роллета, и там же находится механизм управления. </p>
+						</div>
+					</div>
+					<div class="col-md-6">
+						<div class="row asProductRow">
+							<div class="col-md-12">
+								<div class="asOffer">
+									<a class="asOffer__link" href="#">
+										<figure class="asOffer__figure">
+											<div class="asOffer__img">
+												<img class="img-responsive" src="uploads/your_city-rollety/pic_1.jpg" alt="Защитные роллеты">
+											</div>
+											<figcaption>Защитные роллеты</figcaption>
+										</figure>
+									</a>
+									<div class="asPrice">
+										Цена от
+										<span class="asPrice__count">2900</span>
+										<span class="asPrice__currency">грн</span>
+									</div>
+								</div>
+							</div>
+						</div>		
+					</div>
+				</div>
+				<div class="lg-pt-25 lg-pb-10">
+					<?php include 'contents/regional/your_city-rollety/slider--rollety-zashitnye.php'; ?>
+				</div>
+				<p class="asProductBlock__text">Пластиковые окна в Киеве и Киевской области предлагают и другие компании, но если вы стремитесь сделать помещение теплым и уютным, подарить безопасность себе и своим близким - выбирайте “Окна КОРСА”. Наши торговые точки расположены в таких городах Киевской области: Белая Церковь, Ирпень, Киев, Обухов, Сквира, Фастов.</p>
+			
+			</section>
+
+		</div>
+	</div>
+</div>
+<div class="lg-ptb-40">
+	<?php include 'contents/regional/your_city-rollety/getCall_first.php'; ?>
+</div>
+<div class="container">
+	<div class="row">
+		<div class="col-md-12">
+
+			<section class="asProductBlock">
+				<h2 class="as__subtitle">Роллетные решетки в Киеве</h2>
+				<div class="row">
+					<div class="col-md-6">
+						<div class="asProductBlock__content">
+							<p class="asProductBlock__text">Роллетные решетки ТМ КОРСА – уникальный продукт, разработанный для владельцев торговых площадей. Это аналог защитной роллеты, в которой, вместо сплошных ламелей, используется ячеистый экструдированный алюминиевый профиль. Роллетные решетки Вы можете видеть повсеместно - в  торговых центрах, офисах банков, ресторанах и кафе, на витринах магазинов и бутиков.</p>
+							<p class="asProductBlock__text">Открывают обзор витрины. Даже когда Ваш магазин закрыт, яркая, светящаяся и манящая витрина обязательно привлечет внимание  потенциальных  покупателей и увеличит Ваши продажи. Защищают Вашу торговую площадь, открывая обзор. В то время как видимая людям  витрина привлекает посетителей, Вы можете спокойно отдыхать. Роллетные решетка надежно  защищает от проникновения Ваш бизнес. Гармонично вписываются в интерьер и добавляют привлекательности любому торговому помещению.</p>
+							<p class="asProductBlock__text">Роллетные решетки изготавливаются из экструдированных алюминиевых профилей шириной до 84мм и толщиной до 19мм. Одной секцией роллетной решетки можно защитить витрину до 7 метров длиной и 3 метров высотой. Вы можете выбрать из 19 основных цветов по RAL любой понравившийся.</p>
+						</div>
+					</div>
+					<div class="col-md-6">
+						<div class="row asProductRow">
+							<div class="col-md-12">
+								<div class="asOffer">
+									<a class="asOffer__link" href="#">
+										<figure class="asOffer__figure">
+											<div class="asOffer__img">
+												<img class="img-responsive" src="uploads/your_city-rollety/pic_2.jpg" alt="Роллетные решетки">
+											</div>
+											<figcaption>Роллетные решетки</figcaption>
+										</figure>
+									</a>
+									<div class="asPrice">
+										Цена от
+										<span class="asPrice__count">5850</span>
+										<span class="asPrice__currency">грн</span>
+									</div>
+								</div>
+							</div>
+						</div>		
+					</div>
+				</div>
+				<div class="lg-pt-25 lg-pb-10">
+					<?php include 'contents/regional/your_city-rollety/slider--rollety-zashitnye.php'; ?>
+				</div>
+				<p class="asProductBlock__text">Пластиковые окна в Киеве и Киевской области предлагают и другие компании, но если вы стремитесь сделать помещение теплым и уютным, подарить безопасность себе и своим близким - выбирайте “Окна КОРСА”. Наши торговые точки расположены в таких городах Киевской области: Белая Церковь, Ирпень, Киев, Обухов, Сквира, Фастов.</p>
+			
+			</section>
+
+		</div>
+	</div>
+</div>
+
+
+
+
+<?php include 'contents/reviews.php'; ?>
+<?php include 'contents/regional/your_city/differentTypesGoods.php'; ?>
+
+<?php include 'footers/footer.php'; ?>
