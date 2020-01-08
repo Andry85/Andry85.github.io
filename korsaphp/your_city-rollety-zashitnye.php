69b087b70e33790dbb7045ea37817552a2a3c357
Andry85 <shapovala@ukr.net>
2020-01-08 17:49:41 +0200

    your_city-rollety-zashitnye added page

---
+++ korsaphp/your_city-rollety-zashitnye.php
@@ -0,0 +1,306 @@
+<?php include 'headers/header.php'; ?>
+
+<div class="container">
+	<div class="row">
+		<div class="col-md-12">
+			
+			<section class="asProductDesc">
+				<h1 class="asProductDesc__title">ЗАщитные роллеты и решетки В КИЕВЕ</h1>
+				<ul class="breadcrumb">
+					<li class="breadcrumb__item">
+						<a class="breadcrumb__link" href="index.php"><i class="icons fa fa-home"></i></a> 
+					</li>
+					<li class="breadcrumb__item">
+						<span>купить защитные роллеты и решетки киев</span>
+					</li>
+				</ul>
+				<div class="asProductDesc_text asProductDesc_text--nobotomMargin">
+					<p class="asProductDescText__entry">
+						На сайте нашей компании представлен широкий ассортимент перегородок для офиса, ресторана, магазина, торгового центра, дома и дачи. Мы предлагаем стеклянные, металлопластиковые, алюминиевые перегородки, которые отменно вписываются в любой интерьер. Эстетичные и прочные конструкции изготавливаются нашими заводами с европейским оборудованием. Используются новейшие технологии. Для вашего удобства на сайте представлены ближайшие пункты продаж. С помощью интерактивной карты вы узнаете кратчайший путь к офису. И сможете купить перегородки в Киеве по ценам производителя. Специалисты подробнее расскажут о продукции, ее качестве, особенностях. Помогут определиться с наилучшим выбором. Профессионально ответят на все волнующие вопросы относительно товара и дизайна.
+					</p>
+				</div>
+			</section>
+
+			
+			<?php include 'contents/regional/your_city/whereToBuy.php'; ?>
+			<?php include 'contents/regional/your_city/map.php'; ?>
+
+			<div class="row lg-mb-20">
+				<div class="col-md-12">
+					<div class="asProductDesc_text asProductDesc_text--nobotomMargin">
+						<p class="asProductDescText__entry">Компания Окна КОРСА вышла на рынок в 1997-ом году и сразу же заняла лидирующие позиции благодаря высокому качеству продукции. Лучшим доказательством надежности является обширная база постоянных клиентов, их положительные отзывы. Покупатели, однажды заказав пластиковые перегородки, впоследствии обращаются только в нашу компанию. Офисные перегородки и двери изготавливаются по заказу клиентов, с учетом общего интерьера помещения.</p>	
+					</div>
+				</div>
+			</div>
+
+			<section class="asProductBlock">
+				<h2 class="as__subtitle">Защитные роллеты в Киеве</h2>
+				<div class="row">
+					<div class="col-md-6">
+						<div class="asProductBlock__content">
+							<p class="asProductBlock__text">Роллеты (рольставни) ТМ КОРСА предназначены для дополнительной защиты помещения от взлома, посторонних взглядов,  влияния погодных условий, обеспечивая, в тоже время, ощутимое сбережение энергии. Роллеты устанавливаются снаружи помещения, обычно вплотную к окнам.</p>
+							<p class="asProductBlock__text">Роллеты поднимаются / опускаются путем скручивания роллеты на вал, расположенный в коробе сверху окна. Возможна дополнительная фиксация роллеты в закрытом состоянии путем установления в нижней части ролеты механического замка. Роллеты состоят из профилей (ламелей), короба, в который скручивается роллета, направляющих шин и механизма управления.</p>
+							<p class="asProductBlock__text">Существует несколько вариантов  механизмов управления роллетами: инерционный, шнуровой, воротковый, пружинно-инерционный. Выбор механизма роллет зависит от веса роллетного полотна. Само роллетное полотно состоит из роллетных профилей (ламелей). Роллетные профили делятся на пенозаполенные и экструдированные. Короб и боковые крышки. Короб роллет ТМ КОРСА изготавливается  в различных формах, и может быть выполнен как с углами, так и с закруглением. Внутрь короба сворачивается поднимающаяся роллета, и там же находится механизм управления. </p>
+						</div>
+					</div>
+					<div class="col-md-6">
+						<div class="row asProductRow">
+							<div class="col-md-12">
+								<div class="asOffer">
+									<a class="asOffer__link" href="#">
+										<figure class="asOffer__figure">
+											<div class="asOffer__img">
+												<img class="img-responsive" src="uploads/your_city-rollety/pic_1.jpg" alt="Защитные роллеты">
+											</div>
+											<figcaption>Защитные роллеты</figcaption>
+										</figure>
+									</a>
+									<div class="getPrice getPrice-order">
+										<div class="asPrice">
+											Цена от
+											<span class="asPrice__count">2900</span>
+											<span class="asPrice__currency">грн</span>
+										</div>
+										<a class="js-callModal asBtnLink asBtnLink-order" href="#" title="Заказать">Заказать</a>
+									</div>
+								</div>
+							</div>
+						</div>		
+					</div>
+				</div>
+				<div class="lg-pt-25 lg-pb-10">
+					<?php include 'contents/regional/your_city-rollety/slider--rollety-zashitnye.php'; ?>
+				</div>
+			
+			</section>
+
+		</div>
+	</div>
+</div>
+
+
+<div class="profiles">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<h2 class="as__subtitle">Профиль роллетных ламелей</h2>
+			</div>	
+		</div>
+		<div class="row">	
+			<div class="col-md-6">
+				<div class="profilesBlock">
+					<figure class="profilesBlock__pic">
+						<img class="img-responsive" src="uploads/your_city-rollety-zashitnye/rollety-profil-penozapolnennyj-korsa.jpg" alt="">
+						<figcaption>Пенозаполненный профиль</figcaption>
+					</figure>
+					<p>Пенозаполненный профиль,  представляет собой каркас, из тонкого алюминиевого сплава, заполненный в середине вспененным полиуретаном. Высота ламелей роллет, по желанию клиента, может составлять от 39 до 55 мм. Роллеты ТМ КОРСА из пенозаполненного профиля, являются самым экономичным решением благодаря невысокой цене комплектующих.</p>
+					<p>Роллеты ТМ КОРСА из пенозаполненных ламелей обеспечивают: защиту от нежелательных взглядов, защиту от непогоды, дополнительную тепло и шумоизоляцию, невысокую цену.</p>
+				</div>
+			</div>
+			<div class="col-md-6">
+				<div class="profilesBlock">
+					<figure class="profilesBlock__pic">
+						<img class="img-responsive" src="uploads/your_city-rollety-zashitnye/rollety-profil-ekstrudirovannyj-korsa.jpg" alt="">
+						<figcaption>Экструдированный профиль</figcaption>
+					</figure>
+					<p>Экструдированный профиль,  представляет собой каркас из жесткого алюминиевого сплава, с ребрами жесткости и усиливающими перемычками. Конструкция экструдированного роллетного профиля обеспечивает высокие показатели жесткости.</p>
+					<p>Высота профилей  может быть в одном из трех размеров:  42, 44 и 55 мм отличающиеся прочностью и внешним видом. Максимальную защиту обеспечивает комплектация 55мм экструдированного роллетного профиля с усиленными направляющими шинами.</p>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12">
+				<table class="profilesTable">
+					<thead>
+						<tr>
+							<th>
+								<h2>Решаемые задачи</h2>
+							</th>
+							<th>
+								<h2>Пенозаполненный профиль</h2>
+							</th>
+							<th>
+								<h2>Экструдированный профиль</h2>
+							</th>
+						</tr>
+					</thead>
+  					<tbody>	
+						<tr>
+							<td>
+								<p>Стойкость к взлому</p>
+							</td>
+							<td>
+								<p>Стандартная стойкость к взлому.</p>
+								<p>Для объектов, не требующих дополнительной защиты (коттеджи, частные дома, админ. помещения)</p>
+							</td>
+							<td>
+								<p>Повышенная стойкость к взлому.</p>
+								<p>Для объектов, требующих дополнительную защиту (аптеки, офисы, банки, торговые помещения)</p>
+							</td>
+						</tr>
+						<tr>
+							<td>
+								<p>Защита от посторонних взглядов</p>
+							</td>
+							<td>
+								<p>Да</p>
+							</td>
+							<td>
+								<p>Да</p>
+							</td>
+						</tr>
+						<tr>
+							<td>
+								<p>Защита от солнца или непогоды</p>
+							</td>
+							<td>
+								<p>Да</p>
+							</td>
+							<td>
+								<p>Да</p>
+							</td>
+						</tr>
+						<tr>
+							<td>
+								<p>Экономия энергии за счет теплоизоляции</p>
+							</td>
+							<td>
+								<p>Да<br>+20% к эффективности окон</p>
+							</td>
+							<td>
+								<p>Нет</p>
+							</td>
+						</tr>
+						<tr>
+							<td>
+								<p>Выбор формы короба и цветовой гаммы роллеты</p>
+							</td>
+							<td>
+								<p>Да<br>До 20 цветов по RAL</p>
+							</td>
+							<td>
+								<p>Да<br>До 20 цветов по RAL</p>
+							</td>
+						</tr>
+					</tbody>	
+				</table>
+			</div>	
+		</div>
+	</div>
+</div>
+
+<div class="lg-ptb-40 sm-pt-0 sm-pb-30">
+	<?php include 'contents/regional/your_city-rollety/getCall_first.php'; ?>
+</div>
+
+<div class="lg-pt-25 lg-pb-65">
+	<div class="manualControl">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<h2 class="as__subtitleBold">Ручное управление роллетами</h2>
+					<ul class="manualControlList">
+						<li class="manualControlList__item">
+							<figure>
+								<img class="img-responsive" src="uploads/your_city-rollety-zashitnye/manual_control-01.jpg" alt="">
+							</figure>
+							<h3>Пружинно-инерционный механизм</h3>
+							<p>Роллеты легко поднимаются вручную и быстро опускаются под собственным весом. Легкий подъем роллет происходит за счет того, что вес полотна уравновешивается торсионной пружиной, установленной в валу.</p>
+						</li>
+						<li class="manualControlList__item">
+							<figure>
+								<img class="img-responsive" src="uploads/your_city-rollety-zashitnye/manual_control-02.jpg" alt="">
+							</figure>
+							<h3>Ленточный привод</h3>
+							<p>Самый простой вид привода. Полотно ролле ты поднимается и опускается за счет натяжения ленты. Такой привод устанавливается на роллеты с весом полотна до 15 кг.</p>
+						</li>
+						<li class="manualControlList__item">
+							<figure>
+								<img class="img-responsive" src="uploads/your_city-rollety-zashitnye/manual_control-03.jpg" alt="">
+							</figure>
+							<h3>Карданный привод</h3>
+							<p>При вращении воротка привода по часовой стрелке полотно поднимается, при вращении против часовой стрелки – опускается. С помощью карданного привода можно поднять роллеты с весом полотна до 30 кг.</p>
+						</li>
+						<li class="manualControlList__item">
+							<figure>
+								<img class="img-responsive" src="uploads/your_city-rollety-zashitnye/manual_control-04.jpg" alt="">
+							</figure>
+							<h3>Шнуровой привод</h3>
+							<p>Шнуровой привод приходит в действие при вращении рукоятки шнурового укладчика. Устанавливается на роллеты с весом полотна до 20 кг.</p>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="automaticShutter">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<h2 class="as__subtitleBold">Автоматическое управление роллетами</h2>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-8">
+				<p>Благодаря автоматическим вариантам рольставней от Alutech, обеспечены удобное управление изделиями различных габаритов и безопасность. Приводы отличают небольшие размеры, плавные приведение в работу и остановка, устойчивый к действиям вандалов, корпус. Нагрузка на механизмы минимальна, что способствует продлению
+ срока эксплуатации изделий за счёт предотвращения быстрого износа.</p>
+ 				<p>В механизме предусмотрено электронное отслеживание, что позволяет контролировать все компоненты, участвующими в процессе. Конструктивно комплекс управления представляет собой блок (простой выключатель, пульт, либо замок с ключом) с электроприводом и приёмщиком частотного сигнала, активирующего устройства. Данная схема – главная, но на её базе создаются другие разновидности автоматического управления роллетами.</p>
+			</div>
+			<div class="col-md-4">
+				<figure class="automaticShutter__pic">
+					<img class="img-responsive" src="uploads/your_city-rollety-zashitnye/automatic-shutter.jpg" alt="">
+				</figure>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12">
+				<ul class="automaticShutterList">
+					<li class="automaticShutterList__item">
+						<figure>
+							<img class="img-responsive" src="uploads/your_city-rollety-zashitnye/automaticShutter-01.jpg" alt="">
+						</figure>
+						<h3>Клавишный выключатель встраиваемый</h3>
+						<p>самый экономный вариант для управления роллетной конструкцией с электроприводом. Устанавливается внутри помещения, либо при наличии запасного выхода</p>
+					</li>
+					<li class="automaticShutterList__item">
+						<figure>
+							<img class="img-responsive" src="uploads/your_city-rollety-zashitnye/automaticShutter-02.jpg" alt="">
+						</figure>
+						<h3>Радиоуправление с пульта</h3>
+						<p>обеспечивает дистанционное, комфортное управление роллетными изделиями различных габаритов</p>
+					</li>
+					<li class="automaticShutterList__item">
+						<figure>
+							<img class="img-responsive" src="uploads/your_city-rollety-zashitnye/automaticShutter-03.jpg" alt="">
+						</figure>
+						<h3>Замковый выключатель SAPF</h3>
+						<p>наиболее распространенный способ управления роллетами в торгово-развлекательных центрах, складских и промышленных помещениях. Устанавливают как снаружи, так и внутри помещения</p>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</div>
+
+
+
+<div class="lg-pt-20 sm-pt-0">
+	<?php include 'contents/our_offices.php'; ?>
+</div>
+
+<div class="container">
+	<div class="row lg-pt-60">
+		<div class="col-md-12">
+			<?php include 'contents/regional/your_city-rollety/videoCatalog.php'; ?>
+		</div>
+	</div>
+</div>
+
+<?php include 'contents/regional/your_city-rollety/portfolio.php'; ?>
+<?php include 'contents/reviews.php'; ?>
+<?php include 'contents/recommends.php'; ?>
+<?php include 'contents/regional/your_city-rollety/navi.php'; ?>
+
+<?php include 'footers/footer.php'; ?>
