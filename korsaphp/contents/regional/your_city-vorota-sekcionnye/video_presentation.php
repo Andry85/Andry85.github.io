3eec0b6adf675db58722ba7e37c8a9732b089679
Andry85 <shapovala@ukr.net>
2020-01-08 11:28:53 +0200

    your_city-vorota-sekcionnye - fixed page

--- korsaphp/contents/regional/your_city-vorota-sekcionnye/video_presentation.php
+++ korsaphp/contents/regional/your_city-vorota-sekcionnye/video_presentation.php
@@ -5,7 +5,6 @@
 			<div class="presentationList__item">
 				<iframe width="560" height="315" src="https://www.youtube.com/embed/1fE4vfzibK4" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 				<h3 class="presentationList__title">Все про секционные ворота за 2 мин</h3>
-				<a class="presentationList__link" href="https://www.youtube.com/watch?v=1fE4vfzibK4" target="_blank">https://www.youtube.com/watch?v=1fE4vfzibK4</a>
 			</div>
 		</div>
 		<div class="col-md-6">
@@ -14,22 +13,18 @@
 					<div class="presentationList__item">
 						<iframe width="560" height="315" src="https://www.youtube.com/embed/T2nBxS5Gdjw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 						<h3 class="presentationList__title">Презентация - секционные ворота Hörmann</h3>
-						<a class="presentationList__link" href="https://youtu.be/T2nBxS5Gdjw" target="_blank">https://youtu.be/T2nBxS5Gdjw</a>
 					</div>
 					<div class="presentationList__item">
 						<iframe width="560" height="315" src="https://www.youtube.com/embed/5t-bDge7ZXA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 						<h3 class="presentationList__title">Секционные промышленные ворота</h3>
-						<a class="presentationList__link" href="https://www.youtube.com/watch?v=5t-bDge7ZXA" target="_blank">https://www.youtube.com/watch?v=5t-bDge7ZXA</a>
 					</div>
 					<div class="presentationList__item">
 						<iframe width="560" height="315" src="https://www.youtube.com/embed/TK3cTsFIhVc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 						<h3 class="presentationList__title">Как выбрать ворота для Вашего дома Hormann ворота и приводы</h3>
-						<a class="presentationList__link" href="https://www.youtube.com/watch?v=TK3cTsFIhVc&feature=youtu.be" target="_blank">https://www.youtube.com/watch?v=TK3cTsFIhVc&feature=youtu.be</a>
 					</div>
 					<div class="presentationList__item">
 						<iframe width="560" height="315" src="https://www.youtube.com/embed/tnS3bM0poJg" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 						<h3 class="presentationList__title">Секционные ворота</h3>
-						<a class="presentationList__link" href="https://www.youtube.com/watch?v=tnS3bM0poJg&feature=youtu.be" target="_blank">https://www.youtube.com/watch?v=tnS3bM0poJg&feature=youtu.be</a>
 					</div>
 				</div>
 			</div>
