b57e10000c0abfd93e12bcaefbf5c62b17c90e60
Andry85 <shapovala@ukr.net>
2020-01-22 16:33:18 +0200

    fixed

--- korsaphp/your_city-peregorodki-steklyannye.php
+++ korsaphp/your_city-peregorodki-steklyannye.php
@@ -60,7 +60,7 @@
 					</div>
 				</div>
 			</section>
-			<?php include 'contents/advantagesSecond.php'; ?>
+			<?php include 'contents/regional/your_city-okna-alyuminievye/advantagesSecond.php'; ?>
 			<div class="row typesSlider-alyuminievye">
 				<div class="col-md-12">
 					<?php include 'contents/regional/your_city-peregorodki-steklyannye/typesSlider--second.php'; ?>
