1774ee6e18c984afeca2c0a647ae9d6b27b5ca0d
Andrii Shapoval <andrii.shapoval@smile-ukraine.com>
2020-02-15 23:05:53 +0200

    added metalicheslie dveri

---
+++ korsaphp/contents/regional/your_city-dveri-vhodnye-metallicheskie-horman/door_opening_options.php
@@ -0,0 +1,38 @@
+<section class="asGoods asGoods--withoutTopLine asGoods--metallicheskie">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<h2 class="asGoods__title">Мы предлагаем</h2>
+				<p class="asGoods__subtitle">Компания Hörmann предлагает наружные двери из стального полотна и алюминиевой коробки, а также алюминиевые входные двери с алюминиевой коробкой</p>
+				<ul class="listType_4">
+					<li class="listType_4__item">
+						<div class="listType_4__header">
+							<h3 class="listType_4__title"><a href="#">Стальные двери входные</a></h3>
+							<figure class="listType_4__img">
+								<a href="#">
+									<img src="uploads/door_opening/001.jpg" alt="">
+								</a>
+							</figure>
+						</div>
+						<div class="listType_4__footer">
+							<p class="listType_4__text">Для частных домов и квартир. Отличная защита, теплоизоляция и  варианты оформления.</p>
+						</div>		
+					</li>
+					<li class="listType_4__item">
+						<div class="listType_4__header">
+							<h3 class="listType_4__title"><a href="#">Алюминиевые двери входные</a></h3>
+							<figure class="listType_4__img">
+								<a href="#">
+									<img src="uploads/door_opening/002.jpg" alt="">
+								</a>
+							</figure>
+						</div>
+						<div class="listType_4__footer">
+							<p class="listType_4__text">Для технологичных и энергоэффективных домов. Современный дизайн, невероятные эксплуатационные свойства</p>
+						</div>		
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</section>
---
+++ korsaphp/your_city-dveri-vhodnye-metallicheskie-horman.php
@@ -0,0 +1,129 @@
+<?php include 'headers/header.php'; ?>
+
+<div class="container">
+	<div class="row">
+		<div class="col-md-12">
+			
+			<section class="asProductDesc">
+				<h1 class="asProductDesc__title">Входные стальные и алюминиевые Двери в Киеве</h1>
+				<ul class="breadcrumb">
+					<li class="breadcrumb__item">
+						<i class="icons fa fa-home"></i> 
+					</li>
+					<li class="breadcrumb__item">
+						<span>купить входные металлические двери киев</span>
+					</li>
+				</ul>
+				<div class="asProductDesc_text asProductDesc_text--nobotomMargin">
+					<p class="asProductDescText__entry">
+					Сделать ваш дом действительно современным, а дизайн помещений актуальными помогут межкомнатные двери Хорман. Оптимальное сочетание цены и качества, новейшие технологические разработки, смелые дизайнерские решения – все это удовлетворит самого взыскательного покупателя и идеально впишется в интерьер как городской квартиры, так и частного дома. Чтобы выбрать идеальные для вашего помещения межкомнатные деревянные двери в Киеве, нужно на интерактивной карте, размещенной на сайте компании, найти ближайшую точку продаж, где можно получить профессиональную консультацию, ознакомиться с широким выбором дверей из дерева, дверей с элементами остекления, декоративными вставками, либо другими видами отделки, соответствующих выбранному вами стилю. 
+					</p>
+					<p class="asProductDescText__entry">Наша компания является официальным представителем компании Hörmann в Украине. С момента создания в 1997 году, компания Корса выстроила и развила крупнейшую торговую сеть в Украине, но главное, заслужила блестящую репутацию, как среди партнеров, так и среди покупателей. </p>
+				</div>
+			</section>
+			
+			<?php include 'contents/regional/your_city-dveri-vhodnye-metallicheskie-horman/whereToBuy/whereToBuy.php'; ?>
+			<?php include 'contents/regional/your_city/map.php'; ?>
+
+			<div class="lg-pt-30 lg-pb-15">
+				<?php include 'contents/regional/your_city-dveri-vhodnye-metallicheskie-horman/door_opening_options.php'; ?>
+			</div>
+						
+			<section id="odds-section">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-10 section-title">
+							<h2>Преимущества межкомнатных дверей H<b>ö</b>rmann</h2>
+						</div>
+					</div>
+
+					<div class="row">
+						<div class="col-sm-6 col-lg-3">
+							<div class="odds-item">
+								<img src="img/icons/vorota-hermann.png" alt="icon">
+								<h3 class="odds-item__title">Немецкое качество продукции</h3>
+								<p class="odds-item__body">
+									Межкомнатные двери Hörmann изготавливаются в Германии. Они отвечают всем требованиям по качеству и безопасности, действующим в Европе.
+								</p>
+							</div>
+						</div>
+						<div class="col-sm-6 col-lg-3">
+							<div class="odds-item">
+								<img src="img/icons/econom.png" alt="icon">
+								<h3 class="odds-item__title">Межкомнатные двери на долгие годы</h3>
+								<p class="odds-item__body">
+									Мы убеждены в нашей продукции и в нашем качестве. Поэтому межкомнатные двери будут радовать вас на протяжении многих лет
+								</p>
+							</div>
+						</div>
+						<div class="col-sm-6 col-lg-3">
+							<div class="odds-item">
+								<img src="img/icons/design.png" alt="icon">
+								<h3 class="odds-item__title">Покрытие ДВЕРЕЙ Duradecor</h3>
+								<p class="odds-item__body">
+									Для заказа доступны 3 варианта поверхности Decograin и 4 варианта цветов по шкале RAL, а также в 6 эксклюзивных цветах Hörmann Matt Deluxe, подобных цвету гаражных ворот
+								</p>
+							</div>
+						</div>
+						<div class="col-sm-6 col-lg-3">
+							<div class="odds-item">
+								<img src="img/icons/kalitka.png" alt="icon">
+								<h3 class="odds-item__title">скрытый ПРОФИЛь СТВОРКИ двери</h3>
+								<p class="odds-item__body">
+									Изысканное дверное полотно, сплошное с внешней и внутренней стороны, с расположенным внутри профилем створки отвечает самым высоким требованиям к оформлению. Их вид изнутри идеально сочетается с межкомнатными дверями.
+								</p>
+							</div>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-sm-6 col-lg-3">
+							<div class="odds-item">
+								<img src="img/icons/anti-vor.png" alt="icon">
+								<h3 class="odds-item__title">широкий ассортимент продукции</h3>
+								<p class="odds-item__body">
+									Комбинация дверного полотна и коробки на любой вкус. У нас Вы найдете подходящий вариант исполнения двери для Вашего конкретного интерьера. 
+								</p>
+							</div>
+						</div>	
+						<div class="col-sm-6 col-lg-3">
+							<div class="odds-item">
+								<img src="img/icons/ideas.jpg" alt="icon">
+								<h3 class="odds-item__title">Стеклянные двери с нанопокрытием</h3>
+								<p class="odds-item__body">
+									Двери серии GlassLine имеют нанопокрытие, обеспечивающее защиту от отпечатков пальцев. Наши деревянные и стеклянные двери прекрасно комбинируются друг с другом.
+								</p>
+							</div>
+						</div>
+						<div class="col-sm-6 col-lg-3">
+							<div class="odds-item">
+								<img src="img/icons/secure_glazing_icon.png" alt="icon">
+								<h3 class="odds-item__title">Поверхность с Высокой ударопрочностью</h3>
+								<p class="odds-item__body">
+									Поверхность Duradecor отличается повышенной ударопрочностью на 48% более высокой, чем в среднем у прошедших испытание непрерывного давления пластиков других производителей. 
+								</p>
+							</div>
+						</div>
+						<div class="col-sm-6 col-lg-3">
+							<div class="odds-item">
+								<img src="img/icons/multitool.png" alt="icon">
+								<h3 class="odds-item__title">Привод для дверей PortaMatic</h3>
+								<p class="odds-item__body">
+									Для большего комфорта или при наличии физических ограничений, рекомендуюем установить привод, который открывает и закрывает двери автоматически с помощью пульта.
+								</p>
+							</div>
+						</div>
+					</div>
+
+				</div>
+			</section>
+
+			<?php include 'contents/regional/your_city-dveri-vhodnye-metallicheskie/videoCatalog.php'; ?>	
+		</div>
+	</div>
+</div>
+<?php include 'contents/reviews.php'; ?>
+<?php include 'contents/regional/your_city-dveri-vhodnye-metallicheskie/portfolio.php'; ?>
+<?php include 'contents/our_offices.php'; ?>
+<?php include 'contents/regional/your_city-dveri/differentTypesGoodsExamples.php'; ?>
+
+<?php include 'footers/footer.php'; ?>
